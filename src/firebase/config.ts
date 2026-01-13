d7a3ceefaf1378030830da98590480fab6eca316
Moviebox Voicemax <mov33events@gmail.com>
2026-01-13 16:10:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7581249891-88980",
+  "appId": "1:103134509538:web:4b56d973f9652f011a61bb",
+  "apiKey": "AIzaSyCJfiCGNgjIzV6AZQ1r9kXLqSm9RY0JyHU",
+  "authDomain": "studio-7581249891-88980.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "103134509538"
+};
